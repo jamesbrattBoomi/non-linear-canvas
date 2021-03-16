3fb65e669c940df910f17a4ffa4fc269ca7031e2
jamesbrattBoomi <james_bratt@dell.com>
2021-03-16 15:48:12 +0000

    FLOW-3173 - Rendering svgs

--- Flowcanvas.js
+++ Flowcanvas.js
@@ -1,6 +1,19 @@
 import React from "react"
 
-const data = require('./data.json')
+const data = require('./data.json');
+
+const COLOURS = {
+    input: 'rgb(103, 165, 231)',
+    step: 'rgb(98, 183, 237)',
+    decision: 'rgb(124, 207, 96)',
+    operator: 'rgb(95, 204, 100)',
+    message: 'rgb(84, 196, 115)',
+    return: 'rgb(158, 158, 158)',
+    database_save: 'rgb(235, 104, 127)',
+    database_load: 'rgb(248, 125, 118)',
+    database_delete: 'rgb(217, 88, 121)',
+    START: '#34b59d',
+}
 
 const Flowcanvas = () => {
 
@@ -51,38 +64,42 @@ const Flowcanvas = () => {
         return elements;
     }
 
-    /*const generateRoute = (e) => {
-        const elements = [];
+    const rects = [];
 
-        const generate = (elem) => {
-            if (elem.outcomes) {
-                return elem.outcomes.map(outcome => {
-                    const nextElement = data.mapElements.find(m => m.id === outcome.nextMapElementId);
-    
-                    let children = [];
-    
-                    if (nextElement.elementType === 'input') {
-                        children = generateRoute(nextElement);
-                    }
-    
-                    elements.push({ element: nextElement, children });
-                    if (nextElement.elementType !== 'input') {
-                        generate(nextElement);
-                    }
+    const renderFlow = (branches, x, y) => {
+        let xCounter = x;
+        let yCounter = y;
+
+        branches.elements.forEach(e => {
+            const fill = COLOURS[e.element.elementType];
+            const domNode = <rect x={xCounter} y={y} width="100px" height="40px" fill={fill} />
+
+            rects.push(domNode);
+
+            xCounter = xCounter + 110;
+
+            if (e.branches.length > 0) {
+
+                e.branches.sort((a, b) => {
+                    return b.elements.length - a.elements.length;
+                });
+
+                e.branches.forEach(b => {
+                    yCounter = yCounter + 50
+                    renderFlow(b, xCounter, yCounter)
                 })
             }
-        }
-
-        generate(e);
-        return elements;
-    }*/
+        })
+    }
 
     const startElement = data.mapElements.find(m => m.elementType === 'START');
     const flowData = generateRoute(startElement, [], null)
-    console.log(flowData)
+    renderFlow({ name: 'root', elements: flowData }, 0, 0)
 
     return (
-        <svg fill="red" style={{ width: '1000px', height: '1000px' }}></svg>
+        <svg fill="red" style={{ width: '1000px', height: '1000px' }}>
+            {rects}
+        </svg>
     )
 }
 
